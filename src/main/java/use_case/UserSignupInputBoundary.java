425c78ba922d81295315960f5fcbff71af950ada
Geting Qin <janice20030701@gmail.com>
2024-07-10 22:55:12 -0400

    following paulgries loginCleanArchitecture, wrote draft for the user signup output, input data and output, input boundary. Also add user signup interactor.

---
+++ src/main/java/use_case/UserSignupInputBoundary.java
@@ -0,0 +1,5 @@
+package use_case;
+
+public interface UserSignupInputBoundary {
+    void execute(UserSignupInputData signupInputData);
+}
--- src/main/java/use_case/UserSignupInputData.java
+++ src/main/java/use_case/UserSignupInputData.java
@@ -1,2 +1,26 @@
-package use_case;public class UserSignupInputData {
+package use_case;
+
+public class UserSignupInputData {
+
+    final private String username;
+    final private String password;
+    final private String repeatPassword;
+
+    public UserSignupInputData(String username, String password, String repeatPassword) {
+        this.username = username;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
 }
