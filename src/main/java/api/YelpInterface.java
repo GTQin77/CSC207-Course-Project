d92ad9f2e43ac8b20eef7b50f5c52c2ac6aa4ad7
Geting Qin <yizhouzhishang.tea@gmail.com>
2024-07-15 18:27:17 -0400

    Implemented Yelp Api (part2)

--- src/main/java/api/YelpInterface.java
+++ src/main/java/api/YelpInterface.java
@@ -1,4 +1,11 @@
 package api;
+import entity.Business;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 public interface YelpInterface {
+    String getBusinessID(String category, ArrayList<Float> location);
+    Business getBusiness(String businessID);
+    JSONArray getBusinessReviews(String businessID);
 }
