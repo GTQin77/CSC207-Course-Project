4faba81aef4c15449d8db449952c472e069de4db
Joshua Camacho <shua.camacho@gmail.com>
2024-07-18 19:40:55 -0400

    Add DayplanViewModel methods
    Add ViewModel abstract methods

--- src/main/java/interface_adapter/DayplanViewModel.java
+++ src/main/java/interface_adapter/DayplanViewModel.java
@@ -1,6 +1,35 @@
 package interface_adapter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class DayplanViewModel extends ViewModel {
-    public final String TITLE_LABEL = "Prompt Input";
-    public final String PROMPT = "Describe how your day is in a sentence:";
+    public final String titleLabel = "Prompt Input";
+    public final String prompt = "Describe how your day is in a sentence:";
+    public String userInput;
+
+
+    public DayplanViewModel() {
+        super("Dayplan VM");
+    }
+
+    /**
+     * Gets String userPrompt from DayplanViewModel
+     * */
+    public String getPrompt() {
+        return this.userInput;
+    }
+
+    /**
+     * Set the userInput from console
+     * */
+    public void setUserInput() {
+        try {
+            BufferedReader inputData = new BufferedReader(new InputStreamReader(System.in));
+            this.userInput = inputData.readLine();
+        } catch (IOException reader) {
+            System.out.println("Something went wrong!");
+        }
+    }
 }
