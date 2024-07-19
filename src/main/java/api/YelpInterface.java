120871cb80e272a6cf8d68251832c45f25b1b775
Geting Qin <yizhouzhishang.tea@gmail.com>
2024-07-18 23:31:19 -0400

    proudly announce that the Yelp Fusion API is now tested!

--- src/main/java/api/YelpInterface.java
+++ src/main/java/api/YelpInterface.java
@@ -4,7 +4,7 @@ import org.json.JSONArray;
 import java.util.ArrayList;
 
 public interface YelpInterface {
-    ArrayList<String> getBusinessID(String category, ArrayList<Float> location, Integer i);
+    ArrayList<String> getBusinessID(String category, String city, Integer i);
     ArrayList<Object> getBusiness(String businessID);
     JSONArray getBusinessReviews(String businessID);
 }
