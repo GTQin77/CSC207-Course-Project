d6034f462fbc4540cddf34e79bfc10c69ae42332
Geting Qin <yizhouzhishang.tea@gmail.com>
2024-07-21 19:48:15 -0400

    implement signup controller and viewmodel

--- src/main/java/interface_adapter/ViewModel.java
+++ src/main/java/interface_adapter/ViewModel.java
@@ -1,5 +1,13 @@
 package interface_adapter;
 
+
+/**
+ * View model of the program.
+ * <p>
+ * This implementation referenced the Pualgries' Clean Architecture code for ViewModel on
+ * <a href="https://github.com/paulgries/LoginCleanArchitecture/blob/main/src/interface_adapter/ViewModel.java">github.com</a>.
+ * </p>
+ */
 public abstract class ViewModel {
 
     private final String modelName;
