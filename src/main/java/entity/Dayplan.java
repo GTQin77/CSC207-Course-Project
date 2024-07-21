a748501f5301303ffb78321e275c2e4e823a7063
Amelia Wu <ameliawu26@gmail.com>
2024-07-21 03:05:34 -0400

    Fixed Dayplan Entity

--- src/main/java/entity/Dayplan.java
+++ src/main/java/entity/Dayplan.java
@@ -1,29 +1,37 @@
 package entity;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Dayplan {
-    private String vibe;
     private User user;
+    private ArrayList<Double> location;
+    private int numMeals;
+    private int numActivities;
+    private String description;
+    private String vibe;
     private ArrayList<Business> plan;
 
-    public Dayplan() {
-        plan = new ArrayList<>();
-    }
-    public String getVibe() {
-        return vibe;
-    }
-    public void setVibe(String vibe) {
-        this.vibe = vibe;
-    }
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
-        this.user = user;
-    }
-    public ArrayList<Business> getPlan() {
-        return plan;
+    public Dayplan() {plan = new ArrayList<>();}
 
-    }
+    public void setUser(User user) {this.user = user;}
+    public User getUser() {return this.user;}
+
+    public void setLocation(ArrayList<Double> location){this.location = location;}
+    public ArrayList<Double> getLocation(){return this.location;}
+
+    public void setNumMeals(int numMeals){this.numMeals = numMeals;}
+    public int getNumMeals(){return this.numMeals;}
+
+    public void setnumActivities(int numActivities){this.numActivities = numActivities;}
+    public int getnumActivities(){return this.numActivities;}
+
+    public void setDescription(String description){this.description = description;}
+    public String getDescription(){return this.description;}
+
+    public void setVibe(String vibe) {this.vibe = vibe;}
+    public String getVibe(){return this.vibe;}
+
+    public void setPlan(ArrayList<Business> plan){this.plan = plan;}
+    public ArrayList<Business> getPlan() {return plan;}
 }
