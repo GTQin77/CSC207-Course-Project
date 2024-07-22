fb920988415e0aa885967a3c791a1e9e0371f893
Geting Qin <yizhouzhishang.tea@gmail.com>
2024-07-21 20:37:39 -0400

    implement signup view

--- src/main/java/view/UserSignupView.java
+++ src/main/java/view/UserSignupView.java
@@ -1,4 +1,28 @@
 package view;
 
+import interface_adapter.SignupController;
+import interface_adapter.SignupViewModel;
+
+import java.util.Scanner;
+
 public class UserSignupView {
+    private final SignupViewModel signupViewModel;
+    private final SignupController signupController;
+
+    public UserSignupView(SignupViewModel signupViewModel, SignupController signupController) {
+        this.signupViewModel = signupViewModel;
+        this.signupController = signupController;
+
+        signupViewModel.setUserName();
+        String userName = signupViewModel.getUserName();
+
+        signupViewModel.setPassword();
+        String password = signupViewModel.getPassword();
+
+        signupViewModel.setRepeatedPassword();
+        String repeatedPassword = signupViewModel.getRepeatedPassword();
+
+        signupController.execute(userName, password, repeatedPassword);
+    }
+
 }
