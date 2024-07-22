de03e04999474ce87ea26659a9ed1f12d4012d4c
Geting Qin <yizhouzhishang.tea@gmail.com>
2024-07-21 20:50:16 -0400

    SignupPresenter implemented

--- src/main/java/interface_adapter/SignupPresenter.java
+++ src/main/java/interface_adapter/SignupPresenter.java
@@ -1,4 +1,23 @@
 package interface_adapter;
 
-public class SignupPresenter {
+import use_case.user.UserSignupOutputBoundary;
+import use_case.user.UserSignupOutputData;
+
+public class SignupPresenter implements UserSignupOutputBoundary {
+
+    /**
+     * @param user User object created by the signup interactor.
+     */
+    @Override
+    public void prepareSuccessView(UserSignupOutputData user) {
+        System.out.println("Signup success!");
+    }
+
+    /**
+     * @param error Error message from the signup interactor.
+     */
+    @Override
+    public void prepareFailView(String error) {
+        System.out.println("Signup failed!" + error);
+    }
 }
