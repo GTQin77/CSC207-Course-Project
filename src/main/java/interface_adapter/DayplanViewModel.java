cc65be2f21d5ef603ec49c6cd9b27e2b89131715
Joshua Camacho <shua.camacho@gmail.com>
2024-07-21 20:49:01 -0400

    Add DayplanView interactions with console
    Modified methods in DayplanViewModel

--- src/main/java/interface_adapter/DayplanViewModel.java
+++ src/main/java/interface_adapter/DayplanViewModel.java
@@ -1,37 +1,79 @@
 package interface_adapter;
 
 import java.util.Scanner;
+import entity.User
+import use_case.user.UserSignupOutputData;
 
 public class DayplanViewModel extends ViewModel {
     public final String titleLabel = "Prompt Input";
     public final String prompt = "Describe how your day is in a sentence:";
-    public String userInput;
+    public User user;
+    public String userCity;
+    public int userNumMeals;
+    public int userNumActivities;
+    public String userDescription;
+
 
     public DayplanViewModel() {
         super("Dayplan VM");
     }
 
-    public String getTitleLabel() {
-        return this.titleLabel;
-    }
-
-    public String getPrompt() {
-        return this.prompt;
-    }
-
     /**
      * Gets String userInput from DayplanViewModel
      * */
-    public String getUserInput() {
-        return this.userInput;
+    public String getUserDescription() {
+        return this.userDescription;
     }
 
     /**
-     * Get userInput from the console
+     * Get userDescription from the console
      * */
-    public void setUserInput() {
-        System.out.println(prompt);
+    public void setUserDescription() {
+        System.out.println(this.prompt);
         Scanner input = new Scanner(System.in);
-        this.userInput = input.nextLine();
+        this.userDescription = input.nextLine();
+    }
+
+    /**
+     * Get userCity from the console
+     *
+     * Precondition: user input must be in the full name of the city
+     * */
+    public void setUserCity() {
+        System.out.println("Please enter the full name of the city you are located in:");
+        Scanner input = new Scanner(System.in);
+        this.userCity = input.nextLine();
+    }
+
+    public String getUserCity() {
+        return this.userCity;
+    }
+
+    public void setUserNumMeals() {
+        System.out.println("Please enter the desired number of meals today:");
+        Scanner input = new Scanner(System.in);
+        this.userNumMeals = input.nextByte();
+    }
+
+    public int getUserNumMeals() {
+        return this.userNumMeals;
+    }
+
+    public void setUserNumActivities() {
+        System.out.println("Please enter the desired number of activities today:");
+        Scanner input = new Scanner(System.in);
+        this.userNumActivities = input.nextByte();
+    }
+
+    public int getUserNumActivities() {
+        return this.userNumActivities;
+    }
+
+    public void setUser(UserSignupOutputData user) {
+        this.user = user.getUser();
+    }
+
+    public User getUser() {
+        return this.user;
     }
 }
--- src/main/java/view/DayplanView.java
+++ src/main/java/view/DayplanView.java
@@ -2,24 +2,43 @@ package view;
 
 import interface_adapter.DayplanController;
 import interface_adapter.DayplanViewModel;
+import use_case.user.UserSignupOutputData;
+import entity.User;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class DayplanView {
 
     private final DayplanController dayplanController;
     private final DayplanViewModel dayplanViewModel;
 
-
-    public DayplanView(DayplanController dayplanController, DayplanViewModel dayplanViewModel) {
+    public DayplanView(DayplanController dayplanController, DayplanViewModel dayplanViewModel, UserSignupOutputData user) {
         this.dayplanController = dayplanController;
         this.dayplanViewModel = dayplanViewModel;
 
-        //add code here that takes whatever userInput and creates a new object out of it
-        String prompt = dayplanViewModel.getPrompt();
-        System.out.println(prompt);
-        dayplanViewModel.setUserInput();
-        String userInput = dayplanViewModel.getUserInput();
-        dayplanController.execute(userInput);
+        //automatically get user from signup (for now, login for later)
+
+        dayplanViewModel.setUser(user);
+        User userObject = dayplanViewModel.getUser();
+        String userName = userObject.getUserName();
+        ArrayList<Double> userLocation = userObject.getLocation();
+
+        //input user city
+        dayplanViewModel.setUserCity();
+        String userCity = dayplanViewModel.getUserCity();
+
+        //input user number of meals
+        dayplanViewModel.setUserNumMeals();
+        int userNumMeals = dayplanViewModel.getUserNumMeals();
+
+        //input user number of activities
+        dayplanViewModel.setUserNumActivities();
+        int userNumActivities = dayplanViewModel.getUserNumActivities();
+
+        //input user description
+        dayplanViewModel.setUserDescription();
+        String userDescription = dayplanViewModel.getUserDescription();
+
+        dayplanController.execute(userName, userLocation.toString(), userCity, userNumMeals, userNumActivities, userDescription);
     }
 }
