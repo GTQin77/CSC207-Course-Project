2a547f047e671934752f2f6977f142a27941d15c
ashleychoi <ash.choi@mail.utoronto.ca>
2024-07-29 16:35:55 -0400

    implemented login input/output data and boundary

--- src/main/java/use_case/user_login/UserLoginOutputBoundary.java
+++ src/main/java/use_case/user_login/UserLoginOutputBoundary.java
@@ -1,4 +1,8 @@
 package use_case.user_login;
 
+/**
+ * Output boundary of the user login use case.
+ */
 public interface UserLoginOutputBoundary {
+    void presentLoginResult(UserLoginOutputData outputData);
 }
--- src/main/java/use_case/user_login/UserLoginOutputData.java
+++ src/main/java/use_case/user_login/UserLoginOutputData.java
@@ -1,4 +1,29 @@
 package use_case.user_login;
 
+import entity.User;
+
+/**
+ * Output data of the user login use case.
+ */
 public class UserLoginOutputData {
+    private final User user;
+    private final boolean loginSuccessful;
+
+    /**
+     *
+     * @param user The user object if login is successful, null otherwise.
+     * @param loginSuccessful Whether the login was successful.
+     */
+    public UserLoginOutputData(User user, boolean loginSuccessful) {
+        this.user = user;
+        this.loginSuccessful = loginSuccessful;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public boolean isLoginSuccessful() {
+        return loginSuccessful;
+    }
 }
