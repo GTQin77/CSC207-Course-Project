63e6168137f3a6c8fc44779226e2a5342b23c201
ashleychoi <ash.choi@mail.utoronto.ca>
2024-07-30 18:36:37 -0400

    added Paul Gries reference

--- src/main/java/use_case/user_login/UserLoginInputBoundary.java
+++ src/main/java/use_case/user_login/UserLoginInputBoundary.java
@@ -3,6 +3,8 @@ package use_case.user_login;
 import entity.User;
 
 /**
+ * Implementation design taken from Paul Gries LoginCleanArchitecture
+ * <a href="https://github.com/paulgries/LoginCleanArchitecture/blob/main/src/app/SignupUseCaseFactory.java">github</a>
  * Input boundary of the user login use case.
  */
 public interface UserLoginInputBoundary {
--- src/main/java/use_case/user_login/UserLoginOutputBoundary.java
+++ src/main/java/use_case/user_login/UserLoginOutputBoundary.java
@@ -1,6 +1,10 @@
 package use_case.user_login;
 
 /**
+ * <p>
+ * This implementation referenced the Pualgries' Clean Architecture code for SignupInputBoundary on
+ * <a href="https://github.com/paulgries/LoginCleanArchitecture/blob/main/src/use_case/SignupInputBoundary.java">github.com</a>.
+ * </p>
  * Output boundary of the user login use case.
  */
 public interface UserLoginOutputBoundary {
