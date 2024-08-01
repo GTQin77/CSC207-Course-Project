d26596f5c3a9d4805426564c3dafb8fa2a4de606
Joshua Camacho <shua.camacho@gmail.com>
2024-08-01 19:01:24 -0400

    Add PrevPlanDataAccessObject functionality for reading csv files.
    Method is still incomplete and is not implementing the DataAccessInterface that it is associated with.

--- src/main/java/data_access/PrevPlanDataAccessInterface.java
+++ src/main/java/data_access/PrevPlanDataAccessInterface.java
@@ -1,7 +1,10 @@
 package data_access;
 
 import entity.Dayplan;
+import entity.User;
 
 public interface PrevPlanDataAccessInterface {
     Dayplan getPreviousDayplan();
+
+    void getPreviousDayplan(User user);
 }
