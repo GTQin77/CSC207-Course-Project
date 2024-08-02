c6cd0b976cafe78dc272436caaab10e2ed8d4c29
ashleychoi <ash.choi@mail.utoronto.ca>
2024-08-02 12:52:21 -0400

    deleted my login view model

--- src/main/java/interface_adapter/Login/LoginViewModel.java
+++ src/main/java/interface_adapter/Login/LoginViewModel.java
@@ -1,37 +1,3 @@
 package interface_adapter.Login;
 
-import interface_adapter.ViewModel;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class LoginViewModel extends ViewModel {
-    private String userName;
-    private String password;
-    private ArrayList<Double> location;
-
-    public LoginViewModel() {
-        super("LoginViewModel");
-    }
-
-    public void setUserName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your username: ");
-        this.userName = scanner.nextLine();
-    }
-
-    public String getUserName() {
-        return this.userName;
-    }
-
-    public void setPassword() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your password: ");
-        this.password = scanner.nextLine();
-    }
-
-
-    public String getPassword() {
-        return this.password;
-    }
-}
+public class LoginViewModel {}
