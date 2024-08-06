23fb7d3e2cabff8d49452bb2fafa574b0749805e
Geting Qin <janice20030701@gmail.com>
2024-08-05 21:03:19 -0400

    Add dayplaninput state and DayplanInputViewModel

--- src/main/java/interface_adapter/DayplanInput/DayplanInputState.java
+++ src/main/java/interface_adapter/DayplanInput/DayplanInputState.java
@@ -1,4 +1,89 @@
 package interface_adapter.DayplanInput;
 
 public class DayplanInputState {
+    private String city = "";
+    private String cityError = null;
+    private Integer numMeals = 0;
+    private String numMealsError = null;
+    private Integer numActivities = 0;
+    private String numActivitiesError = null;
+    private String description = "";
+    private String descriptionError = null;
+
+    public DayplanInputState(DayplanInputState copy) {
+        city = copy.city;
+        cityError = copy.cityError;
+        numMeals = copy.numMeals;
+        numMealsError = copy.numMealsError;
+        numActivities = copy.numActivities;
+        numActivitiesError = copy.numActivitiesError;
+        description = copy.description;
+        descriptionError = copy.descriptionError;
+    }
+
+    public DayplanInputState() {}
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCityError() {
+        return cityError;
+    }
+
+    public void setCityError(String cityError) {
+        this.cityError = cityError;
+    }
+
+    public Integer getNumMeals() {
+        return numMeals;
+    }
+
+    public void setNumMeals(Integer numMeals) {
+        this.numMeals = numMeals;
+    }
+
+    public String getNumMealsError() {
+        return numMealsError;
+    }
+
+    public void setNumMealsError(String numMealsError) {
+        this.numMealsError = numMealsError;
+    }
+
+    public Integer getNumActivities() {
+        return numActivities;
+    }
+
+    public void setNumActivities(Integer numActivities) {
+        this.numActivities = numActivities;
+    }
+
+    public String getNumActivitiesError() {
+        return numActivitiesError;
+    }
+
+    public void setNumActivitiesError(String numActivitiesError) {
+        this.numActivitiesError = numActivitiesError;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescriptionError() {
+        return descriptionError;
+    }
+
+    public void setDescriptionError(String descriptionError) {
+        this.descriptionError = descriptionError;
+    }
 }
--- src/main/java/interface_adapter/DayplanInput/DayplanInputViewModel.java
+++ src/main/java/interface_adapter/DayplanInput/DayplanInputViewModel.java
@@ -3,21 +3,33 @@ package interface_adapter.DayplanInput;
 import interface_adapter.ViewModel;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class DayplanInputViewModel extends ViewModel {
+    public final String TITLE_LABEL = "Dayplan Input";
+    public final String CITY_LABEL = "Input City and Province";
+    public final String MEALS_LABEL = "Number of Meals";
+    public final String ACTIVITIES_LABEL = "Number of Activities";
+    public final String DESCRIPTION_LABEL = "Describe Your Day";
+
+    public final String OK_BUTTON_LABEL = "OK";
+
+    private DayplanInputState state = new DayplanInputState();
+
+    public DayplanInputViewModel() {super("dayplan input");}
+
+    public void setState(DayplanInputState state) { this.state = state;     }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
 
-    public DayplanInputViewModel(String viewName) {
-        super(viewName);
-    }
-
-    @Override
     public void firePropertyChanged() {
-
+        support.firePropertyChange("state", null, this.state);
     }
 
-    @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-
+        support.addPropertyChangeListener(listener);
     }
+
+    public DayplanInputState getState() { return state; }
 }
