36355bc03316f3cc2757bdf5e43f863300309080
ashleychoi <ash.choi@mail.utoronto.ca>
2024-08-07 18:20:44 -0400

    Set Up Use Case test files

---
+++ src/test/java/use_case/refresh/RefreshInteractorTest.java
@@ -0,0 +1,17 @@
+package use_case.refresh;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RefreshInteractorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
