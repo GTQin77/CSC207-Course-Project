95a20dc21cd04794c64cfc8d81f98a5c3312a3d9
Joshua Camacho <shua.camacho@gmail.com>
2024-08-07 18:56:55 -0400

    Implemented CommonDayplanFactory testing
    Added new method in CommonDayplanFactory.getBusinessFactory

--- src/test/java/entity/CommonDayplanFactoryTest.java
+++ src/test/java/entity/CommonDayplanFactoryTest.java
@@ -1,42 +1,77 @@
 package entity;
 
+import api.OpenAI;
+import api.OpenInterface;
+import api.YelpFusion;
+import api.YelpInterface;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommonDayplanFactoryTest {
 
+    YelpInterface YelpAPI;
+    OpenInterface OpenAI;
+    BusinessFactory YelpBusinessFactory;
+    CommonDayplanFactory commonDayplanFactoryTest;
+    User testUser;
+    ArrayList<Double> location;
+
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        YelpInterface YelpAPI = new YelpFusion();
+        OpenInterface OpenAI = new OpenAI();
+        BusinessFactory YelpBusinessFactory = new YelpBusinessFactory((YelpFusion) YelpAPI);
+        CommonDayplanFactory commonDayplanFactoryTest = new CommonDayplanFactory();
+        ArrayList<Double> location = new ArrayList<>();
+        location.add(0.0);
+        location.add(0.0);
+        User testUser = new User("testJosh", "testPass",location);
     }
 
     @Test
     void setOpenApi() {
-    }
-
-    @Test
-    void getOpenApi() {
+        commonDayplanFactoryTest.setOpenApi(OpenAI);
+        assertNotNull(commonDayplanFactoryTest.getOpenApi());
     }
 
     @Test
     void setYelpApi() {
-    }
-
-    @Test
-    void getYelpApi() {
+        commonDayplanFactoryTest.setYelpApi(YelpAPI);
+        assertNotNull(commonDayplanFactoryTest.getYelpApi());
     }
 
     @Test
     void setBusinessFactory() {
+        commonDayplanFactoryTest.setBusinessFactory(YelpBusinessFactory);
+        assertNotNull(commonDayplanFactoryTest.getBusinessFactory());
     }
 
     @Test
-    void create() {
+    void createOneMealZeroActivity() {
+        CommonDayplanFactory testFactory = new CommonDayplanFactory();
+        testFactory.setYelpApi(YelpAPI);
+        testFactory.setOpenApi(OpenAI);
+        testFactory.setBusinessFactory(YelpBusinessFactory);
+        Dayplan testDayplan = testFactory.create(testUser,location,"Toronto",1,0,
+                "wahahahah im so funny");
+        assertEquals(1, testDayplan.getPlan().size());
     }
+
+    @Test
+    void createZeroMealOneActivity() {
+        CommonDayplanFactory testFactory = new CommonDayplanFactory();
+        testFactory.setYelpApi(YelpAPI);
+        testFactory.setOpenApi(OpenAI);
+        testFactory.setBusinessFactory(YelpBusinessFactory);
+        Dayplan testDayplan = testFactory.create(testUser,location,"Toronto",0,1,
+                "wahahahah im so funny");
+        assertEquals(testDayplan.getNumMeals(), testDayplan.getPlan().size());
+
+    }
+
 }
