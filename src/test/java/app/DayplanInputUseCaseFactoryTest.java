587c2f1b668ee1a90f63c460a7aa406ec0e59e1f
Joshua Camacho <shua.camacho@gmail.com>
2024-08-08 01:28:48 -0400

    Finish testing for DayplanInputUseCaseFactoryTest

---
+++ src/test/java/app/DayplanInputUseCaseFactoryTest.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DayplanInputUseCaseFactoryTest {
+
+    @Test
+    void create() {
+    }
+}
