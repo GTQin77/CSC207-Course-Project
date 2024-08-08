f6a64f3aae680139b0b27f198a959c368783ae62
Joshua Camacho <shua.camacho@gmail.com>
2024-08-08 01:34:16 -0400

    Finish testing for DayplanInputUseCaseFactoryTest

--- src/test/java/app/DayplanInputUseCaseFactoryTest.java
+++ src/test/java/app/DayplanInputUseCaseFactoryTest.java
@@ -1,11 +1,40 @@
 package app;
 
+import interface_adapter.Dayplan.DayplanViewModel;
+import interface_adapter.DayplanInput.DayplanInputViewModel;
+import interface_adapter.Login.LoginViewModel;
+import interface_adapter.ViewManagerModel;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-class DayplanInputUseCaseFactoryTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import services.UserService;
+import view.DayplanInputView;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DayplanInputUseCaseFactoryTest {
+    private ViewManagerModel viewManagerModel;
+    private LoginViewModel loginViewModel;
+    private DayplanInputViewModel dayplanInputViewModel;
+    private DayplanViewModel dayplanViewModel;
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        viewManagerModel = Mockito.mock(ViewManagerModel.class);
+        loginViewModel = Mockito.mock(LoginViewModel.class);
+        dayplanInputViewModel = Mockito.mock(DayplanInputViewModel.class);
+        dayplanViewModel = Mockito.mock(DayplanViewModel.class);
+        userService = Mockito.mock(UserService.class);
+    }
 
     @Test
-    void create() {
+    void testCreate() {
+        DayplanInputView dayplanInputView = DayplanInputUseCaseFactory.create(viewManagerModel, loginViewModel, dayplanInputViewModel, dayplanViewModel, userService);
+        assertNotNull(dayplanInputView);
     }
+
 }
